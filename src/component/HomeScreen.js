979819a01621b8335ce22349b6888026bbd18ce2
Tasnia Nabila <tasnianabila1@gmail.com>
2023-05-31 23:55:08 -0400

    initial setup

---
+++ src/component/HomeScreen.js
@@ -0,0 +1,15 @@
+
+
+const homeScreen = () => {
+
+
+    return (
+        <div className = "wrapper">
+            <div className="buttonContainer">
+                <button>Create a Flashcard Set</button>
+            </div>
+        </div>
+    );
+}
+
+export default homeScreen;
